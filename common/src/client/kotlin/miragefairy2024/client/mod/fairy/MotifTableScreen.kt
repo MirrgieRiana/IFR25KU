663f65ed80bd90474432e79849be5c1bc8ea7bc3
_ <tacticsrealize2@gmail.com>
2025-08-29 11:38:56 +0900

    !!

--- common/src/client/kotlin/miragefairy2024/client/mod/fairy/MotifTableScreen.kt
+++ common/src/client/kotlin/miragefairy2024/client/mod/fairy/MotifTableScreen.kt
@@ -13,9 +13,22 @@ import io.wispforest.owo.ui.core.OwoUIAdapter
 import io.wispforest.owo.ui.core.Sizing
 import io.wispforest.owo.ui.core.Surface
 import io.wispforest.owo.ui.core.VerticalAlignment
+import miragefairy2024.MirageFairy2024
+import miragefairy2024.ModContext
+import miragefairy2024.client.util.LimitedLabelComponent
+import miragefairy2024.client.util.createOwoToast
 import miragefairy2024.client.util.horizontalSpace
+import miragefairy2024.client.util.registerClientPacketReceiver
+import miragefairy2024.client.util.registerHandledScreen
 import miragefairy2024.client.util.verticalScroll
+import miragefairy2024.client.util.verticalSpace
+import miragefairy2024.mod.fairy.GAIN_FAIRY_DREAM_TRANSLATION
+import miragefairy2024.mod.fairy.GainFairyDreamChannel
 import miragefairy2024.mod.fairy.MotifTableScreenHandler
+import miragefairy2024.mod.fairy.createFairyItemStack
+import miragefairy2024.mod.fairy.motifTableScreenHandlerType
+import miragefairy2024.util.black
+import miragefairy2024.util.darkBlue
 import miragefairy2024.util.invoke
 import miragefairy2024.util.plus
 import miragefairy2024.util.text
@@ -25,6 +38,32 @@ import net.minecraft.client.gui.GuiGraphics
 import net.minecraft.network.chat.Component
 import net.minecraft.world.entity.player.Inventory
 import net.minecraft.world.item.Item
+
+context(ModContext)
+fun initClientMotifTable() {
+
+    // GUI登録
+    motifTableScreenHandlerType.registerHandledScreen { gui, inventory, title -> MotifTableScreen(gui, inventory, title) }
+
+    // パケットハンドラ登録
+    GainFairyDreamChannel.registerClientPacketReceiver { motif ->
+        val itemStack = motif.createFairyItemStack()
+        val component = Containers.horizontalFlow(Sizing.fixed(160), Sizing.fixed(32)).apply {
+            surface(Surface.tiled(MirageFairy2024.identifier("textures/gui/fairy_dream_toast.png"), 160, 32))
+            padding(Insets.of(0, 0, 8, 8))
+            verticalAlignment(VerticalAlignment.CENTER)
+            child(Components.item(itemStack))
+            child(horizontalSpace(6))
+            child(Containers.verticalFlow(Sizing.content(), Sizing.content()).apply {
+                child(Components.label(text { GAIN_FAIRY_DREAM_TRANSLATION().black }))
+                child(verticalSpace(2))
+                child(LimitedLabelComponent(itemStack.hoverName.darkBlue).horizontalSizing(Sizing.fixed(160 - 8 - 16 - 6 - 10 - 8)).margins(Insets.of(0, 0, 4, 0)))
+            })
+        }
+        Minecraft.getInstance().toasts.addToast(createOwoToast(component))
+    }
+
+}
 
 class MotifTableScreen(handler: MotifTableScreenHandler, playerInventory: Inventory, title: Component) : BaseOwoHandledScreen<FlowLayout, MotifTableScreenHandler>(handler, playerInventory, title) {
     override fun createAdapter(): OwoUIAdapter<FlowLayout> = OwoUIAdapter.create(this, Containers::verticalFlow)
