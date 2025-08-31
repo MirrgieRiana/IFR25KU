7a94881a0b88134f16de6d158fcf95365cd27e30
_ <tacticsrealize2@gmail.com>
2025-08-31 15:49:51 +0900

    internal: add: EventRegistry

---
+++ common/src/main/kotlin/miragefairy2024/util/EventRegistry.kt
@@ -0,0 +1,53 @@
+package miragefairy2024.util
+
+
+// EventRegistry
+
+open class EventRegistry<L> {
+    val listeners = mutableSetOf<L>()
+}
+
+fun <L> EventRegistry<L>.register(listener: L) {
+    this.listeners += listener
+}
+
+fun <L> EventRegistry<L>.remove(listener: L) {
+    this.listeners -= listener
+}
+
+fun <L> EventRegistry<L>.observe(onClosed: EventRegistry<() -> Unit>, listener: L) {
+    var onClosedListener: (() -> Unit)? = null
+    onClosedListener = {
+        onClosed.remove(onClosedListener!!)
+        this.remove(listener)
+    }
+    this.register(listener)
+    onClosed.register(onClosedListener)
+}
+
+fun <L> EventRegistry<L>.fire(invoker: (L) -> Unit) {
+    listeners.toList().forEach {
+        invoker(it)
+    }
+}
+
+fun EventRegistry<() -> Unit>.fire() = this.fire { it() }
+
+
+// ObservableValue
+
+class ObservableValue<T>(defaultValue: T) : EventRegistry<(T, T) -> Unit>() {
+    var value: T = defaultValue
+        set(new) {
+            val old = field
+            field = new
+            if (new != old) {
+                fire { it(old, new) }
+            }
+        }
+}
+
+fun <T> ObservableValue<T>.observeAndInitialize(onClosed: EventRegistry<() -> Unit>, listener: (T, T) -> Unit) {
+    this.observe(onClosed, listener)
+    listener(this.value, this.value)
+}
