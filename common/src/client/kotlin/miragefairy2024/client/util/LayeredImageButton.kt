c513fc17551158af8c0be976a79bbb59ef4a4a9a
_ <tacticsrealize2@gmail.com>
2025-09-01 05:05:23 +0900

    internal: add: LayeredImageButton

--- common/src/client/kotlin/miragefairy2024/client/util/LayeredImageButton.kt
+++ common/src/client/kotlin/miragefairy2024/client/util/LayeredImageButton.kt
@@ -9,6 +9,23 @@ import net.minecraft.client.gui.components.Button
 import net.minecraft.client.gui.components.WidgetSprites
 import net.minecraft.network.chat.Component
 
+class LayeredImageButton(
+    width: Int,
+    height: Int,
+    private val backgroundSprites: WidgetSprites,
+    private val foregroundSprites: WidgetSprites,
+) : Button(0, 0, width, height, Component.empty(), {
+    (it as LayeredImageButton).onClick.fire()
+}, DEFAULT_NARRATION) {
+    val onClick = EventRegistry<() -> Unit>()
+
+    override fun renderWidget(guiGraphics: GuiGraphics, mouseX: Int, mouseY: Int, partialTick: Float) {
+        super.renderWidget(guiGraphics, mouseX, mouseY, partialTick)
+        guiGraphics.blitSprite(backgroundSprites.get(true, isHoveredOrFocused), x, y, width, height)
+        guiGraphics.blitSprite(foregroundSprites.get(true, isHoveredOrFocused), x, y, width, height)
+    }
+}
+
 class LayeredImageToggleButton(
     width: Int,
     height: Int,
