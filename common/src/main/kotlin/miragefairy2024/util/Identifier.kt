bf7a657dafbb1dad267d1c070e2bdbd6631b505b
_ <tacticsrealize2@gmail.com>
2025-09-06 20:09:41 +0900

    format: Identifier

--- common/src/main/kotlin/miragefairy2024/util/Identifier.kt
+++ common/src/main/kotlin/miragefairy2024/util/Identifier.kt
@@ -4,15 +4,15 @@ import net.minecraft.core.Registry
 import net.minecraft.resources.ResourceKey
 import net.minecraft.resources.ResourceLocation
 
-val ResourceLocation.string get() = this.toString()
-val ResourceLocation.pathString get() = "${this.namespace}.${this.path}"
-
+fun ResourceLocation(namespace: String, path: String): ResourceLocation = ResourceLocation.fromNamespaceAndPath(namespace, path)
+fun ResourceLocation(path: String): ResourceLocation = ResourceLocation.withDefaultNamespace(path)
 fun String.toIdentifier(): ResourceLocation = ResourceLocation.parse(this)
 
 operator fun String.times(identifier: ResourceLocation) = ResourceLocation(identifier.namespace, this + identifier.path)
 operator fun ResourceLocation.times(string: String) = ResourceLocation(this.namespace, this.path + string)
 
-infix fun <T> ResourceKey<out Registry<T>>.with(value: ResourceLocation): ResourceKey<T> = ResourceKey.create(this, value)
+val ResourceLocation.string get() = this.toString()
+val ResourceLocation.pathString get() = "${this.namespace}.${this.path}"
 
-fun ResourceLocation(namespace: String, path: String): ResourceLocation = ResourceLocation.fromNamespaceAndPath(namespace, path)
-fun ResourceLocation(path: String): ResourceLocation = ResourceLocation.withDefaultNamespace(path)
+
+infix fun <T> ResourceKey<out Registry<T>>.with(value: ResourceLocation): ResourceKey<T> = ResourceKey.create(this, value)
