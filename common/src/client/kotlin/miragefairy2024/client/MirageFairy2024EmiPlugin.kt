fd19cb38c2239b68a19b829f4642768a3af95f8a
_ <tacticsrealize2@gmail.com>
2025-09-14 21:24:35 +0900

    internal: add: onRegisterInformationEntry

--- common/src/client/kotlin/miragefairy2024/client/MirageFairy2024EmiPlugin.kt
+++ common/src/client/kotlin/miragefairy2024/client/MirageFairy2024EmiPlugin.kt
@@ -3,10 +3,18 @@ package miragefairy2024.client
 import dev.emi.emi.api.EmiEntrypoint
 import dev.emi.emi.api.EmiPlugin
 import dev.emi.emi.api.EmiRegistry
+import dev.emi.emi.api.recipe.EmiInfoRecipe
+import dev.emi.emi.api.stack.EmiIngredient
+import miragefairy2024.mod.RecipeEvents
+
 @EmiEntrypoint
 @Suppress("unused")
 class MirageFairy2024EmiPlugin : EmiPlugin {
     override fun register(registry: EmiRegistry) {
-
+        RecipeEvents.onRegisterInformationEntry.fire { listener ->
+            listener {
+                registry.addRecipe(EmiInfoRecipe(it.first.map { ingredient -> EmiIngredient.of(ingredient) }, it.second, it.third))
+            }
+        }
     }
 }
--- common/src/main/kotlin/miragefairy2024/mod/CommonModule.kt
+++ common/src/main/kotlin/miragefairy2024/mod/CommonModule.kt
@@ -36,11 +36,14 @@ import net.minecraft.commands.Commands
 import net.minecraft.core.component.DataComponents
 import net.minecraft.core.registries.Registries
 import net.minecraft.network.RegistryFriendlyByteBuf
+import net.minecraft.network.chat.Component
 import net.minecraft.network.codec.StreamCodec
+import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Items
 import net.minecraft.world.item.alchemy.PotionContents
 import net.minecraft.world.item.alchemy.Potions
+import net.minecraft.world.item.crafting.Ingredient
 import net.minecraft.world.level.block.Blocks
 
 val mirageFairy2024ItemGroupCard = ItemGroupCard(
@@ -60,6 +63,10 @@ val rootAdvancement = AdvancementCard(
 object CommandEvents {
     val onRegisterSubCommand = InitializationEventRegistry<(LiteralArgumentBuilder<CommandSourceStack>) -> LiteralArgumentBuilder<CommandSourceStack>>()
     val onRegisterClientSubCommand = InitializationEventRegistry<(LiteralArgumentBuilder<ClientCommandRegistrationEvent.ClientCommandSourceStack>) -> LiteralArgumentBuilder<ClientCommandRegistrationEvent.ClientCommandSourceStack>>()
+}
+
+object RecipeEvents {
+    val onRegisterInformationEntry = InitializationEventRegistry<((Triple<List<Ingredient>, List<Component>, ResourceLocation>) -> Unit) -> Unit>() // TODO rei implementation
 }
 
 context(ModContext)
