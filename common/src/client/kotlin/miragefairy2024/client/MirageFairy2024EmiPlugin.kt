c052c5e7e10c4efe75e7924c2614ca58061406fd
_ <tacticsrealize2@gmail.com>
2025-09-14 20:44:25 +0900

    Update MirageFairy2024EmiPlugin.kt

--- common/src/client/kotlin/miragefairy2024/client/MirageFairy2024EmiPlugin.kt
+++ common/src/client/kotlin/miragefairy2024/client/MirageFairy2024EmiPlugin.kt
@@ -1,8 +1,12 @@
 package miragefairy2024.client
 
 import dev.emi.emi.api.EmiEntrypoint
+import dev.emi.emi.api.EmiPlugin
+import dev.emi.emi.api.EmiRegistry
 @EmiEntrypoint
 @Suppress("unused")
 class MirageFairy2024EmiPlugin : EmiPlugin {
+    override fun register(registry: EmiRegistry) {
 
+    }
 }
