dee386fff729999291fa5d0e4b3572114a0d0a6b
_ <tacticsrealize2@gmail.com>
2025-10-01 12:03:07 +0900

    refactor: RecipeViewerCategoryCard

--- common/src/main/kotlin/miragefairy2024/mod/recipeviewer/RecipeViewerCategoryCard.kt
+++ common/src/main/kotlin/miragefairy2024/mod/recipeviewer/RecipeViewerCategoryCard.kt
@@ -2,7 +2,7 @@ package miragefairy2024.mod.recipeviewer
 
 import miragefairy2024.ModContext
 
-abstract class RecipeViewerCategoryCard<T> {
+abstract class RecipeViewerCategoryCard<R> {
     context(ModContext)
     open fun init() {
 
