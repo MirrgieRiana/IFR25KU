35d7bca8e1034b2d17a16ef2f966e65376941154
_ <tacticsrealize2@gmail.com>
2025-10-03 09:10:48 +0900

    internal: add: IngredientStack

---
+++ common/src/main/kotlin/miragefairy2024/util/IngredientStack.kt
@@ -0,0 +1,29 @@
+package miragefairy2024.util
+
+import com.mojang.serialization.Codec
+import com.mojang.serialization.codecs.RecordCodecBuilder
+import net.minecraft.tags.TagKey
+import net.minecraft.world.item.Item
+import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.crafting.Ingredient
+
+data class IngredientStack(val ingredient: Ingredient, val count: Int) {
+    companion object {
+        val CODEC: Codec<IngredientStack> = RecordCodecBuilder.create { instance ->
+            instance.group(
+                Ingredient.CODEC.fieldOf("Ingredient").forGetter { it.ingredient },
+                Codec.INT.fieldOf("Amount").forGetter { it.count },
+            ).apply(instance, ::IngredientStack)
+        }
+    }
+}
+
+fun IngredientStack(ingredient: Ingredient) = IngredientStack(ingredient, 1)
+fun IngredientStack(itemStack: ItemStack, amount: Int = 1) = IngredientStack(itemStack.toIngredient(), amount)
+
+fun Ingredient.toIngredientStack(amount: Int = 1) = IngredientStack(this, amount)
+fun ItemStack.toIngredientStack(amount: Int = 1) = IngredientStack(this, amount)
+fun Item.toIngredientStack(amount: Int = 1) = this.defaultInstance.toIngredientStack(amount)
+fun TagKey<Item>.toIngredientStack(amount: Int = 1) = IngredientStack(this.toIngredient(), amount)
+
+fun IngredientStack.toItemStacks(): List<ItemStack> = ingredient.items.map { it.copyWithCount(count) }
