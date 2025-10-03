f5b5e9e6d11adafb7a5c42f3e5d637f9c491c428
_ <tacticsrealize2@gmail.com>
2025-10-04 05:07:12 +0900

    internal: add: ViewPlacerRegistry

--- common/src/main/kotlin/miragefairy2024/mod/recipeviewer/View.kt
+++ common/src/main/kotlin/miragefairy2024/mod/recipeviewer/View.kt
@@ -18,6 +18,26 @@ fun interface ViewPlacer<V : View> {
     fun place(view: V, x: Int, y: Int)
 }
 
+fun interface ContextViewPlacer<C, V : View> {
+    fun place(context: C, view: V, x: Int, y: Int)
+}
+
+class ViewPlacerRegistry<C> {
+    val map = mutableMapOf<Class<out View>, ContextViewPlacer<C, *>>()
+}
+
+inline fun <C, reified V : View> ViewPlacerRegistry<C>.register(factory: ContextViewPlacer<C, V>) {
+    this.map[V::class.java] = factory
+}
+
+fun <C, V : View> ViewPlacerRegistry<C>.place(context: C, view: V, x: Int, y: Int) {
+    val contextViewPlacer = this.map[view.javaClass]
+    if (contextViewPlacer == null) throw IllegalArgumentException("Unsupported view: $view")
+    @Suppress("UNCHECKED_CAST")
+    contextViewPlacer as ContextViewPlacer<C, V>
+    contextViewPlacer.place(context, view, x, y)
+}
+
 class ColorPair(val lightModeArgb: Int, val darkModeArgb: Int) {
     companion object {
         val DARK_GRAY = ColorPair(0xFF404040.toInt(), 0xFFBBBBBB.toInt())
