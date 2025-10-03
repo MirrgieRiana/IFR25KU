c31fe70a2c5d83c9d2775088dd0786474dc69991
_ <tacticsrealize2@gmail.com>
2025-10-03 09:17:05 +0900

    internal: add: ReusableInitializationEventRegistry

--- common/src/main/kotlin/miragefairy2024/Initialization.kt
+++ common/src/main/kotlin/miragefairy2024/Initialization.kt
@@ -21,6 +21,24 @@ class InitializationEventRegistry<T> {
     }
 }
 
+class ReusableInitializationEventRegistry<T> {
+    private val list = mutableListOf<T>()
+    private var frozen = false
+
+    context(ModContext)
+    operator fun invoke(listener: T) {
+        check(!frozen) { "Cannot register listener to already fired initialization event." }
+        list += listener
+    }
+
+    fun fire(processor: (T) -> Unit) {
+        frozen = true
+        list.forEach {
+            processor(it)
+        }
+    }
+}
+
 object Modules {
     private val lock = Any()
     private var initialized = false
