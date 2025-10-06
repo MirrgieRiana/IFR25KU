553c68f6d7d845fb94520bc1ef15ccb5402b59e5
_ <tacticsrealize2@gmail.com>
2025-10-06 12:18:20 +0900

    internal: add: IntPoint, IntRectangle

--- common/src/main/kotlin/miragefairy2024/mod/recipeviewer/View.kt
+++ common/src/main/kotlin/miragefairy2024/mod/recipeviewer/View.kt
@@ -48,4 +48,23 @@ enum class Alignment {
     START, CENTER, END,
 }
 
+data class IntPoint(val x: Int, val y: Int)
+
+fun IntPoint.offset(dx: Int, dy: Int) = IntPoint(x + dx, y + dy)
+operator fun IntPoint.unaryMinus() = IntPoint(-x, -y)
+operator fun IntPoint.plus(other: IntPoint) = IntPoint(x + other.x, y + other.y)
+operator fun IntPoint.minus(other: IntPoint) = IntPoint(x - other.x, y - other.y)
+
+data class IntRectangle(val x: Int, val y: Int, val width: Int, val height: Int)
+
+val IntRectangle.topLeft get() = IntPoint(x, y)
+val IntRectangle.topRight get() = IntPoint(x + width, y)
+val IntRectangle.bottomLeft get() = IntPoint(x, y + height)
+val IntRectangle.bottomRight get() = IntPoint(x + width, y + height)
+fun IntRectangle.offset(dx: Int, dy: Int) = IntRectangle(x + dx, y + dy, width, height)
+fun IntRectangle.grow(d: Int) = this.grow(d, d)
+fun IntRectangle.grow(dw: Int, dh: Int) = IntRectangle(x - dw, y - dh, width + dw * 2, height + dh * 2)
+operator fun IntRectangle.plus(point: IntPoint) = IntRectangle(x + point.x, y + point.y, width, height)
+operator fun IntRectangle.minus(point: IntPoint) = IntRectangle(x - point.x, y - point.y, width, height)
+
 fun View(block: SingleView<View>.() -> Unit): View = SingleView { block(this) }.childView
