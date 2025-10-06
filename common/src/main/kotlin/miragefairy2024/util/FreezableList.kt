abb47fac9e678cfe114be98f08b8b31f4c222f03
_ <tacticsrealize2@gmail.com>
2025-10-06 12:16:50 +0900

    internal: add: FreezableRegistry

--- common/src/main/kotlin/miragefairy2024/util/FreezableList.kt
+++ common/src/main/kotlin/miragefairy2024/util/FreezableList.kt
@@ -24,3 +24,28 @@ class FreezableList<T> {
 }
 
 operator fun <T> FreezableList<T>.plusAssign(item: T) = this.add(item)
+
+
+class FreezableRegistry<K, V> {
+    private val map = mutableMapOf<K, V>()
+    private var cache: Map<K, V>? = null
+
+    fun register(key: K, value: V) {
+        check(cache == null) { "Already frozen." }
+        check(key !in map) { "Key already exists: $key" }
+        map[key] = value
+    }
+
+    fun freezeAndGet(): Map<K, V> {
+        val oldCache = cache
+        return if (oldCache == null) {
+            val newCache = map.toMap()
+            cache = newCache
+            newCache
+        } else {
+            oldCache
+        }
+    }
+}
+
+operator fun <K, V> FreezableRegistry<K, V>.set(key: K, value: V) = this.register(key, value)
