fd9533e3ee6f7a6284f8874184d0594f05c18b01
_ <tacticsrealize2@gmail.com>
2025-10-10 09:47:46 +0900

    internal: Rei: add:

--- common/src/main/kotlin/miragefairy2024/util/Rei.kt
+++ common/src/main/kotlin/miragefairy2024/util/Rei.kt
@@ -1,10 +1,14 @@
 package miragefairy2024.util
 
+import me.shedaniel.math.Dimension
 import me.shedaniel.math.Point
+import me.shedaniel.math.Rectangle
 import me.shedaniel.rei.api.common.entry.EntryIngredient
 import me.shedaniel.rei.api.common.entry.EntryStack
 import me.shedaniel.rei.api.common.util.EntryIngredients
 import me.shedaniel.rei.api.common.util.EntryStacks
+import miragefairy2024.mod.recipeviewer.view.IntPoint
+import miragefairy2024.mod.recipeviewer.view.IntRectangle
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.crafting.Ingredient
 
@@ -26,6 +30,14 @@ fun IngredientStack.toEntryIngredient(): EntryIngredient {
         .mapNotNull { it.copyWithCount(count).toEntryStack() }
         .toEntryIngredient()
 }
+
+
+fun IntPoint.toReiPoint() = Point(x, y)
+fun IntPoint.toReiDimension() = Dimension(x, y)
+fun Point.toIntPoint() = IntPoint(x, y)
+fun Dimension.toIntPoint() = IntPoint(width, height)
+fun IntRectangle.toReiRectangle() = Rectangle(x, y, width, height)
+fun Rectangle.toIntRectangle() = IntRectangle(x, y, width, height)
 
 
 operator fun Point.plus(other: Point) = Point(this.x + other.x, this.y + other.y)
