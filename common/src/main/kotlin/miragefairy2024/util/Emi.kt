ee5a38aa3a7c57a446b9a65338c378647a68040e
_ <tacticsrealize2@gmail.com>
2025-10-10 09:47:31 +0900

    internal: add: IntRectangle.toEmiBounds, Bounds.toIntRectangle

--- common/src/main/kotlin/miragefairy2024/util/Emi.kt
+++ common/src/main/kotlin/miragefairy2024/util/Emi.kt
@@ -2,6 +2,8 @@ package miragefairy2024.util
 
 import dev.emi.emi.api.stack.EmiIngredient
 import dev.emi.emi.api.stack.EmiStack
+import dev.emi.emi.api.widget.Bounds
+import miragefairy2024.mod.recipeviewer.view.IntRectangle
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.crafting.Ingredient
 
@@ -10,3 +12,7 @@ fun ItemStack.toEmiStack(): EmiStack = EmiStack.of(this)
 fun Ingredient.toEmiIngredient(): EmiIngredient = EmiIngredient.of(this)
 fun ItemStack.toEmiIngredient(): EmiIngredient = this.toIngredient().toEmiIngredient()
 fun IngredientStack.toEmiIngredient(): EmiIngredient = EmiIngredient.of(this.ingredient, this.count.toLong())
+
+
+fun IntRectangle.toEmiBounds() = Bounds(x, y, width, height)
+fun Bounds.toIntRectangle() = IntRectangle(x, y, width, height)
