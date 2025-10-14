e0f9c0af5c7838b0c5165ec6a749df785903d34b
_ <tacticsrealize2@gmail.com>
2025-10-14 15:47:48 +0900

    internal: ColorPair: add: DEFAULT

--- common/src/main/kotlin/miragefairy2024/mod/recipeviewer/view/ColorPair.kt
+++ common/src/main/kotlin/miragefairy2024/mod/recipeviewer/view/ColorPair.kt
@@ -2,6 +2,7 @@ package miragefairy2024.mod.recipeviewer.view
 
 class ColorPair(val lightModeArgb: Int, val darkModeArgb: Int) {
     companion object {
+        val DEFAULT = ColorPair(0xFFFFFFFF.toInt(), 0xFFBBBBBB.toInt())
         val DARK_GRAY = ColorPair(0xFF404040.toInt(), 0xFFBBBBBB.toInt())
     }
 }
