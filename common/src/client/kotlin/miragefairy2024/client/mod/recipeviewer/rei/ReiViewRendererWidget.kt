d1faea47a1f7e06ed205174c3865e5e8aa760615
_ <tacticsrealize2@gmail.com>
2025-10-14 18:15:35 +0900

    format: ReiViewRendererWidget

--- common/src/client/kotlin/miragefairy2024/client/mod/recipeviewer/rei/ReiViewRendererWidget.kt
+++ common/src/client/kotlin/miragefairy2024/client/mod/recipeviewer/rei/ReiViewRendererWidget.kt
@@ -10,8 +10,8 @@ import net.minecraft.client.gui.components.events.GuiEventListener
 
 class ReiViewRendererWidget<V : PlaceableView>(private val renderer: ViewRenderer<V>, private val view: V, private val bounds2: IntRectangle) : WidgetWithBounds() {
     private val boundsCache = bounds2.toReiRectangle()
-    override fun children() = listOf<GuiEventListener>()
     override fun getBounds() = boundsCache
+    override fun children() = listOf<GuiEventListener>()
     override fun mouseClicked(mouseX: Double, mouseY: Double, button: Int) = renderer.mouseClicked(view, bounds2, mouseX.toInt(), mouseY.toInt(), button)
     override fun keyPressed(keyCode: Int, scanCode: Int, modifiers: Int) = renderer.keyPressed(view, bounds2, keyCode, scanCode, modifiers)
     override fun render(context: GuiGraphics, mouseX: Int, mouseY: Int, delta: Float) = renderer.render(view, bounds2, context, mouseX, mouseY, delta)
