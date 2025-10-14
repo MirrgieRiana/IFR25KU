2d65345d919d0d6008fac5997ed9fb2c024caffd
_ <tacticsrealize2@gmail.com>
2025-10-14 19:08:14 +0900

    refactor: split: MagicPlantClientModule

---
+++ common/src/client/kotlin/miragefairy2024/client/mod/magicplant/TraitListScreen.kt
@@ -0,0 +1,171 @@
+package miragefairy2024.client.mod.magicplant
+
+import io.wispforest.owo.ui.base.BaseOwoHandledScreen
+import io.wispforest.owo.ui.component.Components
+import io.wispforest.owo.ui.container.Containers
+import io.wispforest.owo.ui.container.FlowLayout
+import io.wispforest.owo.ui.container.ScrollContainer
+import io.wispforest.owo.ui.core.Color
+import io.wispforest.owo.ui.core.HorizontalAlignment
+import io.wispforest.owo.ui.core.Insets
+import io.wispforest.owo.ui.core.OwoUIAdapter
+import io.wispforest.owo.ui.core.Sizing
+import io.wispforest.owo.ui.core.Surface
+import io.wispforest.owo.ui.core.VerticalAlignment
+import miragefairy2024.client.mod.surface
+import miragefairy2024.client.util.ClickableContainer
+import miragefairy2024.client.util.OwoComponent
+import miragefairy2024.client.util.inventoryNameLabel
+import miragefairy2024.client.util.tooltipContainer
+import miragefairy2024.client.util.topBorderLayout
+import miragefairy2024.client.util.verticalScroll
+import miragefairy2024.client.util.verticalSpace
+import miragefairy2024.mod.NinePatchTextureCard
+import miragefairy2024.mod.magicplant.TraitEffectKey
+import miragefairy2024.mod.magicplant.TraitListScreenHandler
+import miragefairy2024.mod.magicplant.TraitStack
+import miragefairy2024.mod.magicplant.contents.getTraitPower
+import miragefairy2024.mod.magicplant.getMagicPlantBlockEntity
+import miragefairy2024.mod.magicplant.getName
+import miragefairy2024.mod.magicplant.style
+import miragefairy2024.mod.magicplant.texture
+import miragefairy2024.mod.magicplant.traitListScreenTranslation
+import miragefairy2024.util.darkGray
+import miragefairy2024.util.invoke
+import miragefairy2024.util.join
+import miragefairy2024.util.plus
+import miragefairy2024.util.register
+import miragefairy2024.util.style
+import miragefairy2024.util.text
+import mirrg.kotlin.hydrogen.formatAs
+import net.minecraft.client.Minecraft
+import net.minecraft.network.chat.Component
+import net.minecraft.world.entity.player.Inventory
+
+class TraitListScreen(handler: TraitListScreenHandler, playerInventory: Inventory, title: Component) : BaseOwoHandledScreen<FlowLayout, TraitListScreenHandler>(handler, playerInventory, title) {
+    private lateinit var traitCardContainer: FlowLayout
+
+    private fun setTraitCardContent(component: OwoComponent?) {
+        traitCardContainer.clearChildren()
+        if (component != null) traitCardContainer.child(component)
+        uiAdapter.inflateAndMount()
+    }
+
+    override fun createAdapter(): OwoUIAdapter<FlowLayout> = OwoUIAdapter.create(this, Containers::verticalFlow)
+    override fun build(rootComponent: FlowLayout) {
+        rootComponent.apply {
+            surface(Surface.VANILLA_TRANSLUCENT)
+            verticalAlignment(VerticalAlignment.CENTER)
+            horizontalAlignment(HorizontalAlignment.CENTER)
+
+            child(Containers.verticalFlow(Sizing.content(), Sizing.content()).apply { // 外枠
+                surface(Surface.PANEL)
+                padding(Insets.of(7))
+
+                child(Containers.verticalFlow(Sizing.fixed(18 * 9), Sizing.content()).apply { // 内枠
+                    child(inventoryNameLabel(text { traitListScreenTranslation() }, HorizontalAlignment.CENTER)) // GUI名
+                    child(verticalSpace(3))
+                    traitCardContainer = Containers.verticalFlow(Sizing.fill(100), Sizing.fixed(18 * 6)).apply { // 特性カードコンテナ
+                        surface(NinePatchTextureCard.TRAIT_BACKGROUND.surface)
+                        padding(Insets.of(5))
+                    }
+                    child(traitCardContainer)
+                    child(verticalSpace(4))
+                    child(tooltipContainer(Sizing.fill(100), Sizing.fixed(18 * 4)).apply { // 特性リスト
+                        child(verticalScroll(Sizing.fill(100), Sizing.fill(100), 5).apply {
+                            scrollbar(ScrollContainer.Scrollbar.flat(Color.ofArgb(0xA0FFFFFF.toInt())))
+
+                            child().child(Containers.verticalFlow(Sizing.fill(100), Sizing.content()).apply {
+                                val player = Minecraft.getInstance().player!!
+                                val level = player.level()
+                                val blockEntity = level.getMagicPlantBlockEntity(menu.blockPos)
+
+                                menu.traitStacks.traitStackList.forEach { traitStack ->
+                                    val totalConditionFactor = traitStack.trait.conditions
+                                        .map { it.getFactor(level, menu.blockPos, blockEntity) }
+                                        .fold(1.0) { a, b -> a * b }
+
+                                    child(ClickableContainer(Sizing.fill(100), Sizing.content()).apply { // 特性
+                                        onClick.register {
+                                            setTraitCardContent(createTraitCardContent(traitStack))
+                                            true
+                                        }
+                                        child(Components.label(text {
+                                            val texts = mutableListOf<Component>()
+                                            val styleFunction: (Component) -> Component = { if (totalConditionFactor == 0.0) it.darkGray else it.style(traitStack.trait.style) }
+                                            texts += styleFunction(traitStack.trait.getName())
+                                            texts += styleFunction(traitStack.level.toString(2)())
+                                            if (traitStack.trait.conditions.isNotEmpty()) texts += traitStack.trait.conditions.map { it.emoji }.join() + " →"()
+                                            if (traitStack.trait.traitEffectKeyEntries.isNotEmpty()) texts += traitStack.trait.traitEffectKeyEntries.map { it.traitEffectKey.emoji.style(it.traitEffectKey.style) }.join()
+                                            texts.join(" "())
+                                        }))
+                                    })
+                                }
+                            })
+                        })
+                    })
+                })
+            })
+        }
+
+        val traitStack = menu.traitStacks.traitStackList.firstOrNull()
+        if (traitStack != null) setTraitCardContent(createTraitCardContent(traitStack))
+    }
+
+    private fun createTraitCardContent(traitStack: TraitStack): OwoComponent {
+        return topBorderLayout(Sizing.fill(100), Sizing.fill(100)).apply {
+            gap = 5
+
+            child1(Containers.verticalFlow(Sizing.fill(100), Sizing.content()).apply {
+                child(Components.label(text { traitStack.trait.getName().style(traitStack.trait.style) }).apply { // 特性名
+                    sizing(Sizing.fill(100), Sizing.content())
+                    horizontalTextAlignment(HorizontalAlignment.CENTER)
+                })
+                child(verticalSpace(5))
+                child(Containers.horizontalFlow(Sizing.fill(100), Sizing.fixed(32)).apply { // 特性アイコン欄
+                    val player = Minecraft.getInstance().player!!
+                    val level = player.level()
+                    val blockEntity = level.getMagicPlantBlockEntity(menu.blockPos)
+
+                    child(Containers.verticalFlow(Sizing.expand(50), Sizing.fill(100)).apply { // 条件
+                        verticalAlignment(VerticalAlignment.BOTTOM)
+
+                        traitStack.trait.conditions.forEach { condition ->
+                            val factor = condition.getFactor(level, menu.blockPos, blockEntity)
+                            val text = text { condition.emoji + " "() + (factor * 100.0 formatAs "%.1f%%")() }
+                            child(Components.label(text).tooltip(condition.name))
+                        }
+                    })
+                    child(Containers.verticalFlow(Sizing.content(), Sizing.fill(100)).apply { // 特性アイコン
+                        child(Components.texture(traitStack.trait.texture, 0, 0, 32, 32, 32, 32))
+                    })
+                    child(Containers.verticalFlow(Sizing.expand(50), Sizing.fill(100)).apply { // 効果
+                        horizontalAlignment(HorizontalAlignment.RIGHT)
+                        verticalAlignment(VerticalAlignment.BOTTOM)
+
+                        val totalConditionFactor = traitStack.trait.conditions
+                            .map { it.getFactor(level, menu.blockPos, blockEntity) }
+                            .fold(1.0) { a, b -> a * b }
+
+                        traitStack.trait.traitEffectKeyEntries.forEach {
+                            fun <T : Any> render(traitEffectKey: TraitEffectKey<T>, traitEffectFactor: Double) {
+                                val value = traitEffectKey.getValue(traitEffectFactor * getTraitPower(traitStack.level) * totalConditionFactor)
+                                val text = text { traitEffectKey.renderValue(value) + " "() + traitEffectKey.emoji.style(traitEffectKey.style) }
+                                child(Components.label(text).tooltip(traitEffectKey.name))
+                            }
+                            render(it.traitEffectKey, it.factor)
+                        }
+                    })
+                })
+            })
+            child2(verticalScroll(Sizing.fill(100), Sizing.fill(100), 5, overlapped = true).apply { // 特性ポエム
+                scrollbar(ScrollContainer.Scrollbar.flat(Color.ofArgb(0xA0FFFFFF.toInt())))
+
+                child().child(Components.label(text { traitStack.trait.poem }).apply {
+                    sizing(Sizing.fill(100), Sizing.content())
+                    horizontalTextAlignment(HorizontalAlignment.LEFT)
+                })
+            })
+        }
+    }
+}
