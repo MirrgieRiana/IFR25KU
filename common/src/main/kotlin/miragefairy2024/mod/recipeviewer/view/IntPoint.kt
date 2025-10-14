2781a775bac49c05868aad3675738fa1906f6f93
_ <tacticsrealize2@gmail.com>
2025-10-14 21:24:03 +0900

    internal: IntPoint: add

--- common/src/main/kotlin/miragefairy2024/mod/recipeviewer/view/IntPoint.kt
+++ common/src/main/kotlin/miragefairy2024/mod/recipeviewer/view/IntPoint.kt
@@ -1,5 +1,8 @@
 package miragefairy2024.mod.recipeviewer.view
 
+import mirrg.kotlin.helium.max
+import mirrg.kotlin.helium.min
+
 data class IntPoint(val x: Int, val y: Int) {
     companion object {
         val ZERO = IntPoint(0, 0)
@@ -12,3 +15,8 @@ fun IntPoint.offset(dx: Int, dy: Int) = this.plus(dx, dy)
 fun IntPoint.minus(dx: Int, dy: Int) = this.plus(-dx, -dy)
 operator fun IntPoint.plus(other: IntPoint) = this.plus(other.x, other.y)
 operator fun IntPoint.minus(other: IntPoint) = this.minus(other.x, other.y)
+infix fun IntPoint.max(other: IntPoint) = IntPoint(x max other.x, y max other.y)
+infix fun IntPoint.min(other: IntPoint) = IntPoint(x min other.x, y min other.y)
+infix fun IntPoint.atLeast(other: IntPoint) = this max other
+infix fun IntPoint.atMost(other: IntPoint) = this min other
+fun IntPoint.sized(size: IntPoint) = IntRectangle(x, y, size.x, size.y)
