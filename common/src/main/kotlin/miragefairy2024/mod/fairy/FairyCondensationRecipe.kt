45fc2cc1ac5880a29f74eab75dea1886e56ad83f
_ <tacticsrealize2@gmail.com>
2025-10-16 00:47:02 +0900

    refactor: FairyCondensationRecipe

--- common/src/main/kotlin/miragefairy2024/mod/fairy/FairyCondensationRecipe.kt
+++ common/src/main/kotlin/miragefairy2024/mod/fairy/FairyCondensationRecipe.kt
@@ -16,11 +16,11 @@ import miragefairy2024.util.isNotIn
 import miragefairy2024.util.plusAssign
 import miragefairy2024.util.registerSpecialRecipe
 import miragefairy2024.util.text
+import miragefairy2024.util.toIngredient
 import mirrg.kotlin.helium.join
 import net.minecraft.core.NonNullList
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Items
-import net.minecraft.world.item.crafting.Ingredient
 import java.math.BigInteger
 
 val FAIRY_DECONDENSATION_RECIPE_TITLE_TRANSLATION = Translation({ MirageFairy2024.identifier("fairy_decondensation.title").toLanguageKey("gui", "description") }, "Decondensing Fairies", "妖精の展開")
@@ -70,7 +70,7 @@ fun initFairyCondensationRecipe() {
     FAIRY_DECONDENSATION_RECIPE_TITLE_TRANSLATION.enJa()
     FAIRY_DECONDENSATION_RECIPE_DESCRIPTION_TRANSLATION.enJa()
     RecipeViewerEvents.informationEntries += InformationEntry(
-        { Ingredient.of(motifRegistry.map { it.createFairyItemStack() }.stream()) },
+        { motifRegistry.map { it.createFairyItemStack() }.toIngredient() },
         text { FAIRY_DECONDENSATION_RECIPE_TITLE_TRANSLATION() },
         listOf(text { FAIRY_DECONDENSATION_RECIPE_DESCRIPTION_TRANSLATION() }),
         MirageFairy2024.identifier("/fairy_decondensation"),
