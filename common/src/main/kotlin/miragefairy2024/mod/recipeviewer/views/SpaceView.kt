afd7ca2a782cf802c1edf6aac2826078684dcaaa
_ <tacticsrealize2@gmail.com>
2025-10-16 00:00:35 +0900

    refactor: SpaceView

--- common/src/main/kotlin/miragefairy2024/mod/recipeviewer/views/SpaceView.kt
+++ common/src/main/kotlin/miragefairy2024/mod/recipeviewer/views/SpaceView.kt
@@ -4,9 +4,14 @@ import miragefairy2024.mod.recipeviewer.view.IntPoint
 import miragefairy2024.mod.recipeviewer.view.PlaceableView
 import miragefairy2024.mod.recipeviewer.view.ViewPlacer
 
-abstract class SpaceView(size: IntPoint) : FixedView(size) {
+class SpaceView(size: IntPoint) : FixedView(size) {
     override fun attachTo(offset: IntPoint, viewPlacer: ViewPlacer<PlaceableView>) = Unit
 }
 
-class XSpaceView(width: Int) : SpaceView(IntPoint(width, 0))
-class YSpaceView(height: Int) : SpaceView(IntPoint(0, height))
+fun SpaceView() = SpaceView(IntPoint.ZERO)
+
+@Suppress("FunctionName")
+fun XSpaceView(sizeX: Int) = SpaceView(IntPoint(sizeX, 0))
+
+@Suppress("FunctionName")
+fun YSpaceView(sizeY: Int) = SpaceView(IntPoint(0, sizeY))
