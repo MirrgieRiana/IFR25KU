507ed6e0592a45d07e359b0144ba3235a222dd71
_ <tacticsrealize2@gmail.com>
2025-10-17 01:54:00 +0900

    refactor: ReiViewPlacers

--- common/src/client/kotlin/miragefairy2024/client/mod/recipeviewer/rei/ReiViewPlacers.kt
+++ common/src/client/kotlin/miragefairy2024/client/mod/recipeviewer/rei/ReiViewPlacers.kt
@@ -2,8 +2,6 @@ package miragefairy2024.client.mod.recipeviewer.rei
 
 import io.wispforest.owo.compat.rei.ReiUIAdapter
 import io.wispforest.owo.ui.container.Containers
-import me.shedaniel.math.Point
-import me.shedaniel.math.Rectangle
 import me.shedaniel.rei.api.client.gui.widgets.WidgetWithBounds
 import me.shedaniel.rei.api.client.gui.widgets.Widgets
 import miragefairy2024.ModContext
@@ -12,7 +10,6 @@ import miragefairy2024.client.mod.recipeviewer.ViewOwoAdapterRegistry
 import miragefairy2024.client.mod.recipeviewer.ViewRendererRegistry
 import miragefairy2024.client.util.OwoComponent
 import miragefairy2024.mod.recipeviewer.view.IntPoint
-import miragefairy2024.mod.recipeviewer.view.IntRectangle
 import miragefairy2024.mod.recipeviewer.view.PlaceableView
 import miragefairy2024.mod.recipeviewer.view.offset
 import miragefairy2024.mod.recipeviewer.view.register
@@ -25,24 +22,25 @@ import miragefairy2024.mod.recipeviewer.views.InputSlotView
 import miragefairy2024.mod.recipeviewer.views.OutputSlotView
 import miragefairy2024.mod.recipeviewer.views.TextView
 import miragefairy2024.util.toEntryIngredient
+import miragefairy2024.util.toReiPoint
 import miragefairy2024.util.toReiRectangle
 
 context(ModContext)
 fun initReiViewPlacers() {
     REI_VIEW_PLACER_REGISTRY.register { widgets, view: InputSlotView, bounds ->
-        widgets place Widgets.createSlot(Point(bounds.x + view.margin, bounds.y + view.margin))
+        widgets place Widgets.createSlot(bounds.offset.offset(view.margin, view.margin).toReiPoint())
             .entries(view.ingredientStack.toEntryIngredient())
             .markInput()
             .backgroundEnabled(view.drawBackground)
     }
     REI_VIEW_PLACER_REGISTRY.register { widgets, view: CatalystSlotView, bounds ->
-        widgets place Widgets.createSlot(Point(bounds.x + view.margin, bounds.y + view.margin))
+        widgets place Widgets.createSlot(bounds.offset.offset(view.margin, view.margin).toReiPoint())
             .entries(view.ingredientStack.toEntryIngredient())
             .markInput()
             .backgroundEnabled(view.drawBackground)
     }
     REI_VIEW_PLACER_REGISTRY.register { widgets, view: OutputSlotView, bounds ->
-        widgets place Widgets.createSlot(Point(bounds.x + view.margin, bounds.y + view.margin))
+        widgets place Widgets.createSlot(bounds.offset.offset(view.margin, view.margin).toReiPoint())
             .entries(view.itemStack.toEntryIngredient())
             .markOutput()
             .backgroundEnabled(view.drawBackground)
@@ -63,7 +61,7 @@ fun initReiViewPlacers() {
         )
     }
     REI_VIEW_PLACER_REGISTRY.register { widgets, view: ArrowView, bounds ->
-        widgets place Widgets.createArrow(Point(bounds.x, bounds.y))
+        widgets place Widgets.createArrow(bounds.offset.toReiPoint())
             .animationDurationMS(view.durationMilliSeconds?.toDouble() ?: -1.0)
     }
     ViewRendererRegistry.registry.subscribe { entry ->
@@ -77,13 +75,13 @@ fun initReiViewPlacers() {
     ViewOwoAdapterRegistry.registry.subscribe { entry ->
         fun <V : PlaceableView> f(entry: ViewOwoAdapterRegistry.Entry<V>) {
             REI_VIEW_PLACER_REGISTRY.register(entry.viewClass) { widgets, view, bounds ->
-                widgets place ReiUIAdapter(Rectangle(bounds.x, bounds.y, view.actualSize.x, view.actualSize.y), Containers::stack).also { adapter ->
+                widgets place ReiUIAdapter(bounds.toReiRectangle(), Containers::stack).also { adapter ->
                     //adapter.rootComponent().allowOverflow(true)
                     val context = object : ViewOwoAdapterContext {
                         override fun prepare() = adapter.prepare()
                         override fun wrap(view: PlaceableView, size: IntPoint): OwoComponent = adapter.wrap(run {
                             val containerWidget = ReiContainerWidget()
-                            REI_VIEW_PLACER_REGISTRY.place(containerWidget, view, IntRectangle(0, 0, size.x, size.y))
+                            REI_VIEW_PLACER_REGISTRY.place(containerWidget, view, IntPoint.ZERO.sized(size))
                             containerWidget.widgets.single() as WidgetWithBounds
                         })
                     }
