baac114082be10225a01721677024dfa78cba730
_ <tacticsrealize2@gmail.com>
2025-10-16 22:44:34 +0900

    !!

--- common/src/main/kotlin/miragefairy2024/mod/recipeviewer/views/FixedView.kt
+++ common/src/main/kotlin/miragefairy2024/mod/recipeviewer/views/FixedView.kt
@@ -3,7 +3,7 @@ package miragefairy2024.mod.recipeviewer.views
 import miragefairy2024.mod.recipeviewer.view.IntPoint
 import miragefairy2024.mod.recipeviewer.view.Sizing
 
-abstract class FixedView(protected val size: IntPoint) : AbstractView() {
+abstract class FixedView(private val size: IntPoint) : AbstractView() {
     override val sizingX = Sizing.WRAP
     override val sizingY = Sizing.WRAP
     override fun calculateContentSize() = size
