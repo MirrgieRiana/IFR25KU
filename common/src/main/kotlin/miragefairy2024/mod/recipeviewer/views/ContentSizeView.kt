08d9e0d76bab9446f0b02ec6927d46e0b84fb5a5
_ <tacticsrealize2@gmail.com>
2025-10-17 15:53:04 +0900

    internal: add: MaxContentSizeView

--- common/src/main/kotlin/miragefairy2024/mod/recipeviewer/views/ContentSizeView.kt
+++ common/src/main/kotlin/miragefairy2024/mod/recipeviewer/views/ContentSizeView.kt
@@ -2,6 +2,15 @@ package miragefairy2024.mod.recipeviewer.views
 
 import miragefairy2024.mod.recipeviewer.view.IntPoint
 import miragefairy2024.mod.recipeviewer.view.max
+import miragefairy2024.mod.recipeviewer.view.min
+
+class MaxContentSizeView(private val size: IntPoint) : WrapperView() {
+    override fun calculateContentSize() = super.calculateContentSize() min size
+}
+
+fun <P> Child<P, *>.maxContentSizeX(x: Int) = this.wrap(MaxContentSizeView(IntPoint(x, 0)))
+fun <P> Child<P, *>.maxContentSizeY(y: Int) = this.wrap(MaxContentSizeView(IntPoint(0, y)))
+
 
 class MinContentSizeView(private val size: IntPoint) : WrapperView() {
     override fun calculateContentSize() = super.calculateContentSize() max size
