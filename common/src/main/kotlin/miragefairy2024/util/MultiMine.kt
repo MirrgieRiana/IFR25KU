41e0ff73963ff376e741df8b75d19d6a0dd88aff
_ <tacticsrealize2@gmail.com>
2025-11-01 16:55:51 +0900

    refactor: MultiMine

--- common/src/main/kotlin/miragefairy2024/util/MultiMine.kt
+++ common/src/main/kotlin/miragefairy2024/util/MultiMine.kt
@@ -43,49 +43,45 @@ abstract class MultiMine(
 
         // 発動
 
-        collect(MineVisitor())
-    }
+        collect(object : Visitor {
+            override fun visit(
+                originalBlockPosList: Iterable<BlockPos>,
+                miningDamage: Double,
+                maxDistance: Int,
+                maxCount: Int?,
+                neighborType: NeighborType,
+                onMine: (BlockPos) -> Unit,
+                region: BlockBox?,
+                canContinue: (BlockPos, BlockState) -> Boolean,
+            ): Boolean {
+                blockVisitor(originalBlockPosList, visitOrigins = false, maxDistance = maxDistance, maxCount = maxCount, neighborType = neighborType) { _, _, toBlockPos ->
+                    if (region != null && toBlockPos !in region) return@blockVisitor false // 範囲外
+                    val blockState = level.getBlockState(toBlockPos)
+                    if (blockState.getDestroySpeed(level, toBlockPos) < 0) return@blockVisitor false // 破壊不能な硬度
+                    canContinue(toBlockPos, blockState)
+                }.forEach fail@{ (_, blockPos) ->
+                    if (toolItemStack.isEmpty) return false // ツールの耐久値が枯渇した
+                    if (toolItemStack.maxDamage - toolItemStack.damageValue <= miningDamage.ceilToInt()) return false // ツールの耐久値が残り僅か
 
-    private inner class MineVisitor : Visitor {
+                    // 採掘を続行
 
-        override fun visit(
-            originalBlockPosList: Iterable<BlockPos>,
-            miningDamage: Double,
-            maxDistance: Int,
-            maxCount: Int?,
-            neighborType: NeighborType,
-            onMine: (BlockPos) -> Unit,
-            region: BlockBox?,
-            canContinue: (BlockPos, BlockState) -> Boolean,
-        ): Boolean {
-            blockVisitor(originalBlockPosList, visitOrigins = false, maxDistance = maxDistance, maxCount = maxCount, neighborType = neighborType) { _, _, toBlockPos ->
-                if (region != null && toBlockPos !in region) return@blockVisitor false // 範囲外
-                val blockState = level.getBlockState(toBlockPos)
-                if (blockState.getDestroySpeed(level, toBlockPos) < 0) return@blockVisitor false // 破壊不能な硬度
-                canContinue(toBlockPos, blockState)
-            }.forEach fail@{ (_, blockPos) ->
-                if (toolItemStack.isEmpty) return false // ツールの耐久値が枯渇した
-                if (toolItemStack.maxDamage - toolItemStack.damageValue <= miningDamage.ceilToInt()) return false // ツールの耐久値が残り僅か
-
-                // 採掘を続行
-
-                val targetBlockState = level.getBlockState(blockPos)
-                if (!canContinue(blockPos, targetBlockState)) return@fail // 採掘時のイベントで条件が外れた
-                val targetHardness = targetBlockState.getDestroySpeed(level, blockPos)
-                if (targetHardness > blockState.getDestroySpeed(level, blockPos)) return@fail // 起点のブロックよりも硬いものは掘れない // TODO
-                if (breakBlockByMagic(toolItemStack, level, blockPos, miner)) {
-                    if (targetHardness > 0) {
-                        val damage = level.random.randomInt(miningDamage)
-                        if (damage > 0) {
-                            toolItemStack.hurtAndBreak(damage, miner, EquipmentSlot.MAINHAND)
+                    val targetBlockState = level.getBlockState(blockPos)
+                    if (!canContinue(blockPos, targetBlockState)) return@fail // 採掘時のイベントで条件が外れた
+                    val targetHardness = targetBlockState.getDestroySpeed(level, blockPos)
+                    if (targetHardness > blockState.getDestroySpeed(level, blockPos)) return@fail // 起点のブロックよりも硬いものは掘れない // TODO
+                    if (breakBlockByMagic(toolItemStack, level, blockPos, miner)) {
+                        if (targetHardness > 0) {
+                            val damage = level.random.randomInt(miningDamage)
+                            if (damage > 0) {
+                                toolItemStack.hurtAndBreak(damage, miner, EquipmentSlot.MAINHAND)
+                            }
                         }
+                        onMine(blockPos)
                     }
-                    onMine(blockPos)
                 }
+                return true
             }
-            return true
-        }
-
+        })
     }
 
 }
