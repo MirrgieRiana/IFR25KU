e1c21e05d93fe3aea977533a7d6ca34dbedf7b64
_ <tacticsrealize2@gmail.com>
2025-11-01 19:10:18 +0900

    internal: add: util/Side

---
+++ common/src/main/kotlin/miragefairy2024/util/Side.kt
@@ -0,0 +1,20 @@
+package miragefairy2024.util
+
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.server.level.ServerPlayer
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.level.Level
+
+object ServerSide
+
+@Suppress("UnusedReceiverParameter")
+val ServerPlayer.serverSide get() = ServerSide
+
+@Suppress("UnusedReceiverParameter")
+val ServerLevel.serverSide get() = ServerSide
+
+@Suppress("unused")
+fun Player.asServerPlayer(serverSide: ServerSide) = this as ServerPlayer
+
+@Suppress("unused")
+fun Level.asServerLevel(serverSide: ServerSide) = this as ServerLevel
