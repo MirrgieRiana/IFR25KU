808d4c6f9f41887d85681c850901b788bdd587e0
_ <tacticsrealize2@gmail.com>
2025-11-15 18:50:18 +0900

    format: TraitConditionCard

--- common/src/main/kotlin/miragefairy2024/mod/magicplant/contents/TraitConditionCard.kt
+++ common/src/main/kotlin/miragefairy2024/mod/magicplant/contents/TraitConditionCard.kt
@@ -35,22 +35,70 @@ enum class TraitConditionCard(
     jaName: String,
     function: (world: Level, blockPos: BlockPos, blockEntity: MagicPlantBlockEntity?) -> Double,
 ) {
-    FLOOR_MOISTURE("floor_moisture", Emoji.FLOOR_MOISTURE, "Floor Moisture", "湿った地面", { world, blockPos, _ -> world.getMoisture(blockPos.below()) }),
-    FLOOR_CRYSTAL_ERG("floor_crystal_erg", Emoji.FLOOR_CRYSTAL_ERG, "Floor Crystal Erg", "鉱物質の地面", { world, blockPos, _ -> world.getCrystalErg(blockPos.below()) }),
-    FLOOR_HARDNESS("floor_hardness", Emoji.FLOOR_HARDNESS, "Floor Hardness", "硬い地面", { world, blockPos, _ -> getFloorHardness(world, blockPos) }),
-    LIGHT("light", Emoji.LIGHT, "Light", "光", { world, blockPos, _ -> (world.lightProxy.getLightLevel(blockPos) - 8 atLeast 0) / 7.0 }),
-    DARKNESS("darkness", Emoji.DARKNESS, "Darkness", "闇", { world, blockPos, _ -> ((15 - world.lightProxy.getLightLevel(blockPos)) - 8 atLeast 0) / 7.0 }),
-    LOW_TEMPERATURE("low_temperature", Emoji.LOW_TEMPERATURE, "Low Temperature", "低い気温", { world, blockPos, _ -> if (world.getBiome(blockPos).temperatureCategory == TemperatureCategory.LOW) 1.0 else 0.0 }),
-    MEDIUM_TEMPERATURE("medium_temperature", Emoji.MEDIUM_TEMPERATURE, "Medium Temperature", "普通の気温", { world, blockPos, _ -> if (world.getBiome(blockPos).temperatureCategory == TemperatureCategory.MEDIUM) 1.0 else 0.0 }),
-    HIGH_TEMPERATURE("high_temperature", Emoji.HIGH_TEMPERATURE, "High Temperature", "高い気温", { world, blockPos, _ -> if (world.getBiome(blockPos).temperatureCategory == TemperatureCategory.HIGH) 1.0 else 0.0 }),
-    LOW_HUMIDITY("low_humidity", Emoji.LOW_HUMIDITY, "Low Humidity", "低い湿度", { world, blockPos, _ -> if (world.getBiome(blockPos).humidityCategory == HumidityCategory.LOW) 1.0 else 0.0 }),
-    MEDIUM_HUMIDITY("medium_humidity", Emoji.MEDIUM_HUMIDITY, "Medium Humidity", "普通の湿度", { world, blockPos, _ -> if (world.getBiome(blockPos).humidityCategory == HumidityCategory.MEDIUM) 1.0 else 0.0 }),
-    HIGH_HUMIDITY("high_humidity", Emoji.HIGH_HUMIDITY, "High Humidity", "高い湿度", { world, blockPos, _ -> if (world.getBiome(blockPos).humidityCategory == HumidityCategory.HIGH) 1.0 else 0.0 }),
-    OUTDOOR("outdoor", Emoji.OUTDOOR, "Outdoor", "屋外", { world, blockPos, _ -> if (blockPos.y >= world.getHeightmapPos(Heightmap.Types.MOTION_BLOCKING, blockPos).y) 1.0 else 0.0 }),
-    SUNSHINE_ENVIRONMENT("sunshine_environment", Emoji.OUTDOOR, "Sunshine Environment", "日照環境", { world, blockPos, _ -> world.lightProxy.getPermanentSkyLightLevel(blockPos) / 15.0 }),
-    NATURAL("natural", Emoji.NATURAL, "Natural", "天然", { _, _, blockEntity -> if (blockEntity?.isNatural() == true) 1.0 else 0.0 }),
-    HIGH_ALTITUDE("high_altitude", Emoji.UP, "High Altitude", "高地", { world, blockPos, _ -> world.getHighAltitudeFactor(blockPos) }),
-    LOW_ALTITUDE("low_altitude", Emoji.DOWN, "Low Altitude", "低地", { world, blockPos, _ -> world.getLowAltitudeFactor(blockPos) }),
+    FLOOR_MOISTURE(
+        "floor_moisture", Emoji.FLOOR_MOISTURE, "Floor Moisture", "湿った地面",
+        { world, blockPos, _ -> world.getMoisture(blockPos.below()) },
+    ),
+    FLOOR_CRYSTAL_ERG(
+        "floor_crystal_erg", Emoji.FLOOR_CRYSTAL_ERG, "Floor Crystal Erg", "鉱物質の地面",
+        { world, blockPos, _ -> world.getCrystalErg(blockPos.below()) },
+    ),
+    FLOOR_HARDNESS(
+        "floor_hardness", Emoji.FLOOR_HARDNESS, "Floor Hardness", "硬い地面",
+        { world, blockPos, _ -> getFloorHardness(world, blockPos) },
+    ),
+    LIGHT(
+        "light", Emoji.LIGHT, "Light", "光",
+        { world, blockPos, _ -> (world.lightProxy.getLightLevel(blockPos) - 8 atLeast 0) / 7.0 },
+    ),
+    DARKNESS(
+        "darkness", Emoji.DARKNESS, "Darkness", "闇",
+        { world, blockPos, _ -> ((15 - world.lightProxy.getLightLevel(blockPos)) - 8 atLeast 0) / 7.0 },
+    ),
+    LOW_TEMPERATURE(
+        "low_temperature", Emoji.LOW_TEMPERATURE, "Low Temperature", "低い気温",
+        { world, blockPos, _ -> if (world.getBiome(blockPos).temperatureCategory == TemperatureCategory.LOW) 1.0 else 0.0 },
+    ),
+    MEDIUM_TEMPERATURE(
+        "medium_temperature", Emoji.MEDIUM_TEMPERATURE, "Medium Temperature", "普通の気温",
+        { world, blockPos, _ -> if (world.getBiome(blockPos).temperatureCategory == TemperatureCategory.MEDIUM) 1.0 else 0.0 },
+    ),
+    HIGH_TEMPERATURE(
+        "high_temperature", Emoji.HIGH_TEMPERATURE, "High Temperature", "高い気温",
+        { world, blockPos, _ -> if (world.getBiome(blockPos).temperatureCategory == TemperatureCategory.HIGH) 1.0 else 0.0 },
+    ),
+    LOW_HUMIDITY(
+        "low_humidity", Emoji.LOW_HUMIDITY, "Low Humidity", "低い湿度",
+        { world, blockPos, _ -> if (world.getBiome(blockPos).humidityCategory == HumidityCategory.LOW) 1.0 else 0.0 },
+    ),
+    MEDIUM_HUMIDITY(
+        "medium_humidity", Emoji.MEDIUM_HUMIDITY, "Medium Humidity", "普通の湿度",
+        { world, blockPos, _ -> if (world.getBiome(blockPos).humidityCategory == HumidityCategory.MEDIUM) 1.0 else 0.0 },
+    ),
+    HIGH_HUMIDITY(
+        "high_humidity", Emoji.HIGH_HUMIDITY, "High Humidity", "高い湿度",
+        { world, blockPos, _ -> if (world.getBiome(blockPos).humidityCategory == HumidityCategory.HIGH) 1.0 else 0.0 },
+    ),
+    OUTDOOR(
+        "outdoor", Emoji.OUTDOOR, "Outdoor", "屋外",
+        { world, blockPos, _ -> if (blockPos.y >= world.getHeightmapPos(Heightmap.Types.MOTION_BLOCKING, blockPos).y) 1.0 else 0.0 },
+    ),
+    SUNSHINE_ENVIRONMENT(
+        "sunshine_environment", Emoji.OUTDOOR, "Sunshine Environment", "日照環境",
+        { world, blockPos, _ -> world.lightProxy.getPermanentSkyLightLevel(blockPos) / 15.0 },
+    ),
+    NATURAL(
+        "natural", Emoji.NATURAL, "Natural", "天然",
+        { _, _, blockEntity -> if (blockEntity?.isNatural() == true) 1.0 else 0.0 },
+    ),
+    HIGH_ALTITUDE(
+        "high_altitude", Emoji.UP, "High Altitude", "高地",
+        { world, blockPos, _ -> world.getHighAltitudeFactor(blockPos) },
+    ),
+    LOW_ALTITUDE(
+        "low_altitude", Emoji.DOWN, "Low Altitude", "低地",
+        { world, blockPos, _ -> world.getLowAltitudeFactor(blockPos) },
+    ),
     ;
 
     val identifier = MirageFairy2024.identifier(path)
