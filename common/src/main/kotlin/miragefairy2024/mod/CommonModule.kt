c64475f99ebf3180d49e8146d161ef19495d8f9b
_ <tacticsrealize2@gmail.com>
2025-11-24 15:01:56 +0900

    ADD: dump_biome_features

--- common/src/main/kotlin/miragefairy2024/mod/CommonModule.kt
+++ common/src/main/kotlin/miragefairy2024/mod/CommonModule.kt
@@ -28,17 +28,25 @@ import miragefairy2024.util.invoke
 import miragefairy2024.util.isIn
 import miragefairy2024.util.register
 import miragefairy2024.util.registerClientDebugItem
+import miragefairy2024.util.registerServerDebugItem
+import miragefairy2024.util.string
 import miragefairy2024.util.temperatureCategory
 import miragefairy2024.util.text
 import miragefairy2024.util.toTextureSource
 import miragefairy2024.util.translate
 import miragefairy2024.util.writeAction
+import mirrg.kotlin.gson.hydrogen.jsonArray
+import mirrg.kotlin.gson.hydrogen.jsonElement
+import mirrg.kotlin.gson.hydrogen.jsonElementOrJsonNull
+import mirrg.kotlin.gson.hydrogen.jsonObject
+import mirrg.kotlin.gson.hydrogen.toJson
 import mirrg.kotlin.helium.join
 import net.fabricmc.fabric.api.recipe.v1.ingredient.CustomIngredient
 import net.fabricmc.fabric.api.recipe.v1.ingredient.CustomIngredientSerializer
 import net.minecraft.commands.CommandSourceStack
 import net.minecraft.commands.Commands
 import net.minecraft.core.BlockPos
+import net.minecraft.core.Holder
 import net.minecraft.core.component.DataComponents
 import net.minecraft.core.registries.Registries
 import net.minecraft.network.RegistryFriendlyByteBuf
@@ -50,8 +58,11 @@ import net.minecraft.world.item.Items
 import net.minecraft.world.item.alchemy.PotionContents
 import net.minecraft.world.item.alchemy.Potions
 import net.minecraft.world.level.Level
+import net.minecraft.world.level.biome.Biome
 import net.minecraft.world.level.block.Blocks
+import net.minecraft.world.level.levelgen.GenerationStep
 import net.minecraft.world.phys.HitResult
+import kotlin.jvm.optionals.getOrNull
 
 val mirageFairy2024ItemGroupCard = ItemGroupCard(
     MirageFairy2024.identifier("miragefairy2024"), "IFR25KU", "IFR25KU",
@@ -123,6 +134,122 @@ fun initCommonModule() {
             *lines.toTypedArray(),
         )
         writeAction(player, "dump_biome_attributes.csv", table.map { line -> line.join(",") + "\n" }.join(""))
+    }
+    registerServerDebugItem("dump_biome_features", Blocks.SPRUCE_SAPLING.toTextureSource(), 0xFFFF00FF.toInt()) { world, player, _, _ ->
+        fun Level.getBiomes() = this.registryAccess()[Registries.BIOME].listElements().toList()
+        fun Holder<*>.getId() = this.unwrapKey().getOrNull()?.location()?.string
+        fun Holder<Biome>.getPlacedFeatureSets() = this.value().generationSettings.features()
+
+        // PlacedFeatureの単純リスト
+        world.getBiomes().map { biome ->
+            jsonObject(
+                "biome" to biome.getId().jsonElementOrJsonNull,
+                "placedFeatures" to biome.getPlacedFeatureSets().map { placedFeatureSet ->
+                    placedFeatureSet.toList().sortedBy { it.getId() }.map { placedFeature ->
+                        placedFeature.getId().jsonElementOrJsonNull
+                    }.jsonArray
+                }.jsonArray,
+            )
+        }.jsonArray.let { writeAction(player, "dump_biome_features.json", it.toJson { setPrettyPrinting() }) }
+
+        // 順序表
+        GenerationStep.Decoration.entries.map { step ->
+
+            val allPlacedFeatures = world.getBiomes().flatMap { biome ->
+                biome.getPlacedFeatureSets().getOrNull(step.ordinal)?.toList()?.map { it.getId() } ?: emptyList()
+            }.distinct().sortedBy { it }
+
+            val table = mutableMapOf<String?, MutableSet<String?>>()
+            infix fun String?.win(other: String?) = table[this]?.contains(other) == true
+            infix fun String?.notWin(other: String?) = !(this win other)
+
+            // まずバイオームによる初期順序関係を収集
+            world.getBiomes().forEach { biome ->
+                val placedFeatureList = biome.getPlacedFeatureSets().getOrNull(step.ordinal)?.toList()?.map { it.getId() } ?: emptyList()
+                (0 until placedFeatureList.size).forEach { a ->
+                    (a + 1 until placedFeatureList.size).forEach { b ->
+                        table.getOrPut(placedFeatureList[a]) { mutableSetOf() } += placedFeatureList[b]
+                    }
+                }
+            }
+
+            // 更新できなくなるまで推移的順序関係（a > b && b > c => a > c）を適用
+            while (true) {
+                var changed = false
+                allPlacedFeatures.forEach { a ->
+                    allPlacedFeatures.forEach { b ->
+                        allPlacedFeatures.forEach { c ->
+                            if (a win b && b win c && a notWin c) {
+                                table.getOrPut(a) { mutableSetOf() } += c
+                                changed = true
+                            }
+                        }
+                    }
+                }
+                if (!changed) break
+            }
+
+            // ソート
+            val sortedPlacedFeatures = try {
+                allPlacedFeatures.sortedWith { a, b ->
+                    when {
+                        a win b -> 1
+                        b win a -> -1
+                        else -> 0
+                    }
+                }
+            } catch (e: IllegalArgumentException) {
+                e.printStackTrace()
+                allPlacedFeatures
+            }
+
+            // すべての要素がそれ以外のすべてに勝つような最小の要素をひたすらドロップアウト
+            //   ABCDE
+            // A 01111 ←これは他すべてのPlacedFeatureに勝つので単独でドロップアウト
+            // B 00011 ←これはCに勝てず、CもBに勝てないので順序関係が定義できない
+            // C 00011 しかしBとCはそれ以外に勝つのでBとCでまとめてドロップアウト
+            // D 00001
+            // E 00000
+            val remainingPlacedFeatures = sortedPlacedFeatures.toMutableList()
+            val placedFeatureClusters = mutableListOf<List<String?>>()
+            while (remainingPlacedFeatures.isNotEmpty()) {
+                var count = 1
+                while (true) {
+
+                    // remainingPlacedFeaturesの最初のcount個の要素が、それら以外のすべてに勝つならドロップアウト
+                    val takes = remainingPlacedFeatures.take(count)
+                    val drops = remainingPlacedFeatures.drop(count)
+                    val allWin = takes.all { take ->
+                        drops.all { drop ->
+                            take win drop
+                        }
+                    }
+                    if (allWin) {
+                        placedFeatureClusters += takes.sortedBy { it }
+                        remainingPlacedFeatures -= takes
+                        break
+                    }
+
+                    count++
+                }
+            }
+
+            jsonObject(
+                "name" to step.name.jsonElement,
+                "placedFeatureClusters" to placedFeatureClusters.map { placedFeatureCluster ->
+                    if (placedFeatureCluster.size == 1) {
+                        placedFeatureCluster.single().jsonElementOrJsonNull
+                    } else {
+                        placedFeatureCluster.associate { a ->
+                            "$a" to jsonObject(
+                                "wins" to placedFeatureCluster.filter { b -> a win b }.map { it.jsonElementOrJsonNull }.jsonArray,
+                                "loses" to placedFeatureCluster.filter { b -> b win a }.map { it.jsonElementOrJsonNull }.jsonArray,
+                            )
+                        }.jsonObject
+                    }
+                }.jsonArray
+            )
+        }.jsonArray.let { writeAction(player, "dump_biome_features_matrix.json", it.toJson { setPrettyPrinting() }) }
     }
 
     // Server
