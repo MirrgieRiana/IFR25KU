0d6865068378db0be51934cd4baa9f5f10efb0a3
_ <tacticsrealize2@gmail.com>
2025-11-28 17:46:12 +0900

    refactor: BiomeModule

--- common/src/main/kotlin/miragefairy2024/mod/biome/BiomeCard.kt
+++ common/src/main/kotlin/miragefairy2024/mod/biome/BiomeCard.kt
@@ -5,6 +5,10 @@ import miragefairy2024.ModContext
 import miragefairy2024.util.AdvancementCard
 import miragefairy2024.util.EnJa
 import miragefairy2024.util.Translation
+import miragefairy2024.util.enJa
+import miragefairy2024.util.generator
+import miragefairy2024.util.registerChild
+import miragefairy2024.util.registerDynamicGeneration
 import miragefairy2024.util.with
 import net.minecraft.core.HolderGetter
 import net.minecraft.core.registries.Registries
@@ -13,14 +17,6 @@ import net.minecraft.tags.TagKey
 import net.minecraft.world.level.biome.Biome
 import net.minecraft.world.level.levelgen.carver.ConfiguredWorldCarver
 import net.minecraft.world.level.levelgen.placement.PlacedFeature
-
-object BiomeCards {
-    val entries = listOf(
-        FairyForestBiomeCard,
-        DeepFairyForestBiomeCard,
-        RetrospectiveCityBiomeCard,
-    )
-}
 
 abstract class BiomeCard(
     path: String,
@@ -37,7 +33,22 @@ abstract class BiomeCard(
 
     context(ModContext)
     open fun init() {
+
+        // バイオームの生成
+        registerDynamicGeneration(key) {
+            createBiome(lookup(Registries.PLACED_FEATURE), lookup(Registries.CONFIGURED_CARVER))
+        }
+
+        // このバイオームをタグに登録
+        tags.forEach { tag ->
+            tag.generator.registerChild(identifier)
+        }
+
+        // 翻訳生成
+        translation.enJa()
+
         advancement?.init()
+
     }
 }
 
--- common/src/main/kotlin/miragefairy2024/mod/biome/BiomeModule.kt
+++ common/src/main/kotlin/miragefairy2024/mod/biome/BiomeModule.kt
@@ -6,12 +6,8 @@ import miragefairy2024.ModContext
 import miragefairy2024.ModEvents
 import miragefairy2024.util.EnJa
 import miragefairy2024.util.enJa
-import miragefairy2024.util.generator
-import miragefairy2024.util.registerChild
-import miragefairy2024.util.registerDynamicGeneration
 import miragefairy2024.util.toBiomeTag
 import net.minecraft.core.Registry
-import net.minecraft.core.registries.Registries
 import net.minecraft.resources.ResourceKey
 import net.minecraft.world.level.biome.Biome
 import net.minecraft.world.level.biome.Climate
@@ -28,24 +24,7 @@ val FAIRY_BIOME_TAG = MirageFairy2024.identifier("fairy").toBiomeTag()
 
 context(ModContext)
 fun initBiomeModule() {
-    FAIRY_BIOME_TAG.enJa(EnJa("Fairy", "妖精"))
-    BiomeCards.entries.forEach { card ->
 
-        // バイオームの生成
-        registerDynamicGeneration(card.key) {
-            card.createBiome(lookup(Registries.PLACED_FEATURE), lookup(Registries.CONFIGURED_CARVER))
-        }
-
-        // このバイオームをタグに登録
-        card.tags.forEach { tag ->
-            tag.generator.registerChild(card.identifier)
-        }
-
-        // 翻訳生成
-        card.translation.enJa()
-
-        card.init()
-    }
     ModEvents.onTerraBlenderInitialized {
 
         // 地上世界用の共通RegionをTerraBlenderに登録
@@ -60,6 +39,13 @@ fun initBiomeModule() {
         })
 
     }
+
+    FAIRY_BIOME_TAG.enJa(EnJa("Fairy", "妖精"))
+
+    FairyForestBiomeCard.init()
+    DeepFairyForestBiomeCard.init()
+    RetrospectiveCityBiomeCard.init()
+
 }
 
 context(ModContext)
