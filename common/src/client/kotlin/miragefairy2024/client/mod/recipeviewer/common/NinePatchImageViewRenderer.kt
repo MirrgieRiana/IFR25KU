6c335bade5d8d61e18ada78ec7a409c673d8681a
_ <tacticsrealize2@gmail.com>
2025-12-11 04:44:25 +0900

    cleanup: NinePatchImageViewRenderer

--- common/src/client/kotlin/miragefairy2024/client/mod/recipeviewer/common/NinePatchImageViewRenderer.kt
+++ common/src/client/kotlin/miragefairy2024/client/mod/recipeviewer/common/NinePatchImageViewRenderer.kt
@@ -26,11 +26,11 @@ object NinePatchImageViewRenderer : ViewRenderer<NinePatchImageView> {
         val x0 = bounds.x
         val x1 = bounds.x + w1
         val x2 = bounds.x + bounds.sizeX - w3
-        val x3 = bounds.x + bounds.sizeX
+        // val x3 = bounds.x + bounds.sizeX
         val y0 = bounds.y
         val y1 = bounds.y + h1
         val y2 = bounds.y + bounds.sizeY - h3
-        val y3 = bounds.y + bounds.sizeY
+        // val y3 = bounds.y + bounds.sizeY
 
         // 角
         graphics.blit(view.textureId, x0, y0, u0.toFloat(), v0.toFloat(), w1, h1, u3, v3)
