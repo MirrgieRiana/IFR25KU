3427ad4237d2729f98c01387bd17ada751a1ccd7
_ <tacticsrealize2@gmail.com>
2025-12-30 13:45:23 +0900

    FIX: initEntityClientModule

--- common/src/client/kotlin/miragefairy2024/client/mod/entity/EntityClientModule.kt
+++ common/src/client/kotlin/miragefairy2024/client/mod/entity/EntityClientModule.kt
@@ -1,24 +1,25 @@
 package miragefairy2024.client.mod.entity
 
+import dev.architectury.registry.client.level.entity.EntityModelLayerRegistry
 import miragefairy2024.ModContext
 import miragefairy2024.client.util.registerEntityRenderer
 import miragefairy2024.mod.entity.AntimatterBoltCard
 import miragefairy2024.mod.entity.ChaosCubeCard
 import miragefairy2024.mod.entity.EtheroballisticBoltCard
-import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry
 import net.minecraft.client.model.geom.ModelLayerLocation
 import net.minecraft.client.model.geom.builders.LayerDefinition
 import net.minecraft.client.model.geom.builders.MeshDefinition
 import net.minecraft.client.model.geom.builders.PartDefinition
 import net.minecraft.resources.ResourceLocation
+import java.util.function.Supplier
 
 context(ModContext)
 fun initEntityClientModule() {
-    EntityModelLayerRegistry.registerModelLayer(AntimatterBoltEntityRenderer.MAIN.entityModelLayer, AntimatterBoltEntityRenderer.MAIN.provider)
+    EntityModelLayerRegistry.register(AntimatterBoltEntityRenderer.MAIN.entityModelLayer, AntimatterBoltEntityRenderer.MAIN.provider)
     AntimatterBoltCard.entityType.registerEntityRenderer(::AntimatterBoltEntityRenderer)
-    EntityModelLayerRegistry.registerModelLayer(ChaosCubeEntityRenderer.ROOT.entityModelLayer, ChaosCubeEntityRenderer.ROOT.provider)
+    EntityModelLayerRegistry.register(ChaosCubeEntityRenderer.ROOT.entityModelLayer, ChaosCubeEntityRenderer.ROOT.provider)
     ChaosCubeCard.entityType.registerEntityRenderer(::ChaosCubeEntityRenderer)
-    EntityModelLayerRegistry.registerModelLayer(EtheroballisticBoltEntityRenderer.MAIN.entityModelLayer, EtheroballisticBoltEntityRenderer.MAIN.provider)
+    EntityModelLayerRegistry.register(EtheroballisticBoltEntityRenderer.MAIN.entityModelLayer, EtheroballisticBoltEntityRenderer.MAIN.provider)
     EtheroballisticBoltCard.entityType.registerEntityRenderer(::EtheroballisticBoltEntityRenderer)
 }
 
@@ -30,7 +31,7 @@ class EntityModelLayerCard(
     private val configurator: (PartDefinition) -> Unit,
 ) {
     val entityModelLayer = ModelLayerLocation(identifier, layerName)
-    val provider = EntityModelLayerRegistry.TexturedModelDataProvider {
+    val provider = Supplier {
         val modelData = MeshDefinition()
         configurator(modelData.root)
         LayerDefinition.create(modelData, textureWidth, textureHeight)
