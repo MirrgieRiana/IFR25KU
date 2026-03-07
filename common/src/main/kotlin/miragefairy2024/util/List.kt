4e332223e5b112544f54bd2e5e4a779a8cbc147f
_ <tacticsrealize2@gmail.com>
2026-03-07 14:05:54 +0900

    Add List.randomOrNull and randomOrThrow
    Introduce two extensions on List<T> that select an element using Minecraft's RandomSource: randomOrNull(random) returns null for empty lists, and randomOrThrow(random) throws NoSuchElementException when the list is empty. Also add the RandomSource import; implementation uses random.nextInt(size).

--- common/src/main/kotlin/miragefairy2024/util/List.kt
+++ common/src/main/kotlin/miragefairy2024/util/List.kt
@@ -1,5 +1,9 @@
 package miragefairy2024.util
 
 import net.minecraft.core.NonNullList
+import net.minecraft.util.RandomSource
 
 fun <T : Any> List<T>.toNonNullList(): NonNullList<T> = this.toCollection(NonNullList.create<T>())
+
+fun <T> List<T>.randomOrNull(random: RandomSource): T? = if (this.isEmpty()) null else this[random.nextInt(this.size)]
+fun <T> List<T>.randomOrThrow(random: RandomSource): T = if (this.isEmpty()) throw NoSuchElementException("List is empty.") else this[random.nextInt(this.size)]
