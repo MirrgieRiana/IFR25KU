9fee748e8ecf75ae600e1681d941c959d430166c
_ <15280246+MirrgieRiana@users.noreply.github.com>
2026-03-09 09:08:41 +0900

    Use IntRectangle x1/x2/y1/y2 in renderer
    Refactor FilledRectangleViewRenderer to use IntRectangle properties x1, x2, y1, y2 for the graphics.fill call instead of manually computing bounds.x + bounds.sizeX and bounds.y + bounds.sizeY. Added the needed imports; behavior unchanged, simplifies coordinate access.

--- common/src/client/kotlin/miragefairy2024/client/mod/recipeviewer/common/FilledRectangleViewRenderer.kt
+++ common/src/client/kotlin/miragefairy2024/client/mod/recipeviewer/common/FilledRectangleViewRenderer.kt
@@ -2,11 +2,15 @@ package miragefairy2024.client.mod.recipeviewer.common
 
 import miragefairy2024.client.mod.recipeviewer.ViewRenderer
 import miragefairy2024.mod.recipeviewer.view.IntRectangle
+import miragefairy2024.mod.recipeviewer.view.x1
+import miragefairy2024.mod.recipeviewer.view.x2
+import miragefairy2024.mod.recipeviewer.view.y1
+import miragefairy2024.mod.recipeviewer.view.y2
 import miragefairy2024.mod.recipeviewer.views.FilledRectangleView
 import net.minecraft.client.gui.GuiGraphics
 
 object FilledRectangleViewRenderer : ViewRenderer<FilledRectangleView> {
     override fun render(view: FilledRectangleView, bounds: IntRectangle, graphics: GuiGraphics, mouseX: Int, mouseY: Int, delta: Float) {
-        graphics.fill(bounds.x, bounds.y, bounds.x + bounds.sizeX, bounds.y + bounds.sizeY, view.color.value)
+        graphics.fill(bounds.x1, bounds.y1, bounds.x2, bounds.y2, view.color.value)
     }
 }
