5fc9a4081e911ccccaf5cccaf7e84326c138fe22
_ <15280246+MirrgieRiana@users.noreply.github.com>
2026-07-25 22:17:21 +0900

    残留物表示を入力数対応に修正するのだ～🌱
    SimpleMachineRecipeViewerCategoryCard の出力計算で、入力数ぶんの残留物を正しく加算するようにしたのだ～🌱 さらに maxStackSize を超える分は分割して追加し、レシピビュー上の表示個数が実挙動に一致するようにしたのだ～✨

--- common/src/main/kotlin/miragefairy2024/mod/machine/SimpleMachineRecipeViewerCategoryCard.kt
+++ common/src/main/kotlin/miragefairy2024/mod/machine/SimpleMachineRecipeViewerCategoryCard.kt
@@ -29,6 +29,7 @@ import miragefairy2024.util.plus
 import miragefairy2024.util.plusAssign
 import miragefairy2024.util.text
 import miragefairy2024.util.toIngredientStack
+import mirrg.kotlin.helium.atMost
 import mirrg.kotlin.helium.stripTrailingZeros
 import mirrg.kotlin.hydrogen.formatAs
 import net.minecraft.core.RegistryAccess
@@ -41,16 +42,22 @@ abstract class SimpleMachineRecipeViewerCategoryCard<R : SimpleMachineRecipe> : 
     override fun getRecipeCodec(registryAccess: RegistryAccess): Codec<R> = getRecipeCard().serializer.codec().codec()
     override fun getInputs(recipeEntry: RecipeEntry<R>) = recipeEntry.recipe.inputs.map { input -> Input(input.ingredient.toIngredientStack(input.count), false) }
     override fun getOutputs(recipeEntry: RecipeEntry<R>): List<ItemStack> {
-        val remainders = mutableListOf<ItemStack>()
+        val remainingItemStacks = mutableListOf<ItemStack>()
         recipeEntry.recipe.inputs.forEach { input ->
             if (input.consumptionChance > 0.0) { // TODO 半端な確率で生成される容器の表示方法
                 val inputItemStack = input.ingredient.items.firstOrNull() ?: return@forEach // TODO 複数マッチする場合の表示方法
-                val remainder = recipeEntry.recipe.getCustomizedRemainder(inputItemStack)
-                if (!remainder.isNotEmpty) return@forEach
-                remainders += remainder
+                val remainingItemStackSample = recipeEntry.recipe.getCustomizedRemainder(inputItemStack)
+                if (remainingItemStackSample.isNotEmpty) {
+                    var remainingItemStackCount = remainingItemStackSample.count * input.count
+                    while (remainingItemStackCount > 0) {
+                        val count = remainingItemStackCount atMost remainingItemStackSample.maxStackSize
+                        remainingItemStacks += remainingItemStackSample.copyWithCount(count)
+                        remainingItemStackCount -= count
+                    }
+                }
             }
         }
-        return recipeEntry.recipe.outputs.toMutableList() + remainders
+        return recipeEntry.recipe.outputs.toMutableList() + remainingItemStacks
     }
 
     abstract fun getRecipeCard(): SimpleMachineRecipeCard<R>
