33c28f52dbf03048018f23a3696d7f2d9fc793d0
_ <15280246+MirrgieRiana@users.noreply.github.com>
2026-08-18 17:50:42 +0900

    COPALツール材質を微調整したのだ～🌱
    COPALのツール材質パラメータを調整して、耐久値を172から182へ、採掘速度を4.5Fから5.0Fへ引き上げたのだ～✨ 攻撃関連値やエンチャント適性は据え置きで、使い勝手だけを少し改善する意図の変更なのだ～🌱

--- common/src/main/kotlin/miragefairy2024/mod/tool/ToolMaterial.kt
+++ common/src/main/kotlin/miragefairy2024/mod/tool/ToolMaterial.kt
@@ -61,7 +61,7 @@ enum class FairyToolMaterials(
     CALCULITE(Tiers.NETHERITE, 2048, 7.0F, 2.0F, 2.0F, 16, { ingredientOf(Shape.GEM, Material.CALCULITE) }),
     NOISE(Tiers.NETHERITE, 101, 8.9F, 1.1F, 1.3F, 3, { ingredientOf(Shape.GEM, Material.NOISE) }),
     HAIMEVISKA_ROSIN(Tiers.WOOD, 73, 1.2F, 0.0F, 0.0F, 16, { ingredientOf(Shape.GEM, Material.HAIMEVISKA_ROSIN) }),
-    COPAL(Tiers.IRON, 172, 4.5F, 1.5F, 1.5F, 19, { ingredientOf(Shape.GEM, Material.COPAL) }),
+    COPAL(Tiers.IRON, 182, 5.0F, 1.5F, 1.5F, 19, { ingredientOf(Shape.GEM, Material.COPAL) }),
     FAIRY_PLASTIC(Tiers.IRON, 966, 3.0F, 2.0F, 0.0F, 24, { ingredientOf(Shape.GEM, Material.FAIRY_PLASTIC) }),
 
     NEUTRONIUM(Tiers.NETHERITE, Int.MAX_VALUE - 100, 8.0F, 3.0F, 3.0F, 10, { Items.BEDROCK.toIngredient() }),
