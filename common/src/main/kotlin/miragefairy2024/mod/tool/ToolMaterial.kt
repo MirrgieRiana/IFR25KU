c34f855f66bd570bf179709ca4696c8e556151a0
_ <15280246+MirrgieRiana@users.noreply.github.com>
2026-08-23 09:35:08 +0900

    ラピスラズリ工具の性能調整したのだ～🌱
    ラピスラズリ製ツールの耐久値を140に引き上げ、エンチャント性を21へ調整したのだ～🌱 併せて、ToolCard の定義順を整理し、変更ログの説明文から過去の強いエンチャント傾向の記述を削除したのだ～✨

--- common/src/main/kotlin/miragefairy2024/mod/tool/ToolMaterial.kt
+++ common/src/main/kotlin/miragefairy2024/mod/tool/ToolMaterial.kt
@@ -33,7 +33,7 @@ enum class FairyToolMaterials(
 ) : Tier {
     COPPER(Tiers.IRON, 196, 5.0F, 1.0F, 2.0F, 18, { ingredientOf(Shape.INGOT, Material.COPPER) }),
     GLASS(Tiers.STONE, 8, 6.0F, 4.0F, 3.5F, 14, { ConventionalItemTags.GLASS_BLOCKS.toIngredient() }),
-    LAPIS_LAZULI(Tiers.STONE, 128, 4.0F, 1.0F, 1.5F, 30, { ingredientOf(Shape.GEM, Material.LAPIS) }),
+    LAPIS_LAZULI(Tiers.STONE, 140, 4.0F, 1.0F, 1.5F, 21, { ingredientOf(Shape.GEM, Material.LAPIS) }),
     AMETHYST(Tiers.IRON, 218, 5.5F, 2.0F, 2.5F, 23, { ingredientOf(Shape.SHARD, Material.AMETHYST) }),
     OBSIDIAN(Tiers.IRON, 804, 5.0F, 4.5F, 3.0F, 20, { ConventionalItemTags.OBSIDIANS.toIngredient() }),
     EMERALD(Tiers.DIAMOND, 1192, 7.0F, 2.5F, 2.0F, 16, { ingredientOf(Shape.GEM, Material.EMERALD) }),
