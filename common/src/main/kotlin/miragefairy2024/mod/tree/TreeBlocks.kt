bbd0641e0668541b73300b11816b42fda74c27e8
_ <15280246+MirrgieRiana@users.noreply.github.com>
2026-09-08 08:27:32 +0900

    プラノキ滴下原木のロジン枠を樹液に差し替えたのだ～🌱
    プラノキの滴下原木で、ロジン枠に使っていたハイメヴィスカ樹脂のプレースホルダーをやめて、`plastic_tree_sap` に統一したのだ～✨ `TreeBlocks.kt` と `DrippingPlasticTreeLogBlock` の `getRosinItem()`、生成済み loot table のドロップ先を同じ内容にそろえて、TODO も将来のレアドロップ追加方針へ更新したのだ～🌱

--- common/src/main/kotlin/miragefairy2024/mod/tree/TreeBlocks.kt
+++ common/src/main/kotlin/miragefairy2024/mod/tree/TreeBlocks.kt
@@ -256,7 +256,7 @@ abstract class TreeBlockCard(val configuration: TreeBlockConfiguration) {
             PoemList(1)
                 .poem(EnJa("High mana-conducting carbon isotope.", "魔力を帯びたスクロース。"))
                 .description(EnJa("Harvest sap when used", "使用時、樹液を収穫")),
-        ).block { { DrippingPlasticTreeLogBlock(it) } }.drippingLog({ PLASTIC_TREE_LOG }, { MaterialCard.PLASTIC_TREE_SAP.item() }, { MaterialCard.HAIMEVISKA_ROSIN.item() }) // TODO プラノキは涙を持たないから、ハイメヴィスカのものをプレースホルダーとして置いてあるのだ～🌱
+        ).block { { DrippingPlasticTreeLogBlock(it) } }.drippingLog({ PLASTIC_TREE_LOG }, { MaterialCard.PLASTIC_TREE_SAP.item() }, { MaterialCard.PLASTIC_TREE_SAP.item() }) // TODO レアドロップを追加するのだ～🌱
         val PLASTIC_TREE_SAPLING = !TreeBlockConfiguration(
             PLASTIC_TREE_TREE_CONFIGURATION, "plastic_tree_sapling", EnJa("Plastic Tree Sapling", "プラノキの苗木"),
             PoemList(1).poem(EnJa("Etherify sugars with astral radiation.", "天空のフォリオニクス。")),
--- common/src/main/kotlin/miragefairy2024/mod/tree/contents/plastictree/PlasticTreeBlocks.kt
+++ common/src/main/kotlin/miragefairy2024/mod/tree/contents/plastictree/PlasticTreeBlocks.kt
@@ -41,7 +41,7 @@ class DrippingPlasticTreeLogBlock(settings: Properties) : DrippingLogBlock(setti
 
     override fun getIncisedLogBlock() = TreeBlockCard.INCISED_PLASTIC_TREE_LOG.block()
     override fun getSapItem() = MaterialCard.PLASTIC_TREE_SAP.item()
-    override fun getRosinItem() = MaterialCard.HAIMEVISKA_ROSIN.item() // TODO プラノキは涙を持たないから、ハイメヴィスカのものをプレースホルダーとして置いてあるのだ～🌱
+    override fun getRosinItem() = MaterialCard.PLASTIC_TREE_SAP.item() // TODO レア素材を指定するのだ～🌱
 
     override fun animateTick(state: BlockState, level: Level, pos: BlockPos, random: RandomSource) {
         spawnDrippingSapParticle(state, level, pos, random) // TODO パーティクルの種類も、湧く位置も、ハイメヴィスカと共通のものをそのまま使っているのだ～🌱
